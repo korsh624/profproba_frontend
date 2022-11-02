68633ca0bfea578f9005e8cadbd9b969b06a0e82
korsh624 <korsh624@gmail.com>
2022-11-02 12:51:16 +0300

    add v js

--- data/script.js
+++ data/script.js
@@ -2,8 +2,33 @@
 let post='';
 let passwd='';
 const email =document.querySelector('[name=email]');
-    console.log(email);
-    email.addEventListener('input',(event)=>{
-        post=event.target.value;
-        console.log(post);
+const subm=document.querySelector('[name=button]');
+
+console.log(email);
+email.addEventListener('input',(event)=>{
+    post=event.target.value;
+    console.log(post);
+});
+
+const pass =document.querySelector('[name=pass]');
+    console.log(pass);
+    pass.addEventListener('input',(event)=>{
+        passwd=event.target.value;
+        console.log(passwd);
+    });
+
+const bubutton = document.querySelector('.button');
+bubutton.addEventListener('click', (event) => {
+    console.log(event);
+    //стопим перезагрузку страницы
+    event.preventDefault();
+    //Проверяем почту на пустоту
+    if (post===''){
+        email.style.border='1px solid red';
+        bubutton.style.background="red"
+
+        }
+    if (post===''){
+        pass.style.border='1px solid red';
+        }
     });
